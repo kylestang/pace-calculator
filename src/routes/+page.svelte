2a78e4903e767a7bf81b02fbd47fbfc1cca15ba9
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2023-10-17 13:43:25 -0700

    Added github corner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+  import Github from './Github.svelte';
   import PaceCalculator from './PaceCalculator.svelte';
   import { Heading } from 'flowbite-svelte';
 </script>
 
 <div class="h-screen w-full bg-neutral-100">
+  <Github />
+
   <div class="p-4 sm:p-6 flex flex-col items-center w-full">
     <Heading tag="h1" class="w-fit text-primary-700 mb-8">Run Pace Calculator</Heading>
     <PaceCalculator />
